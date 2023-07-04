4842c5a8084e691c125bd36703c21b63c73218aa
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-07-04 12:21:51 +0300

    FLYGO1-5 v2

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -4,14 +4,14 @@ const Landing = () => {
   const imageStyle = {
     width: "100%", 
     height: "700px", 
-    backgroundImage: "../airplane.png", 
+    backgroundImage: "./LandingImg/landingimg.png", 
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain",
    
   };
   return (
     <div>
-      <img src="../airplane.png" alt="Airplane" style ={imageStyle} />
+      <img src="./LandingImg/landingimg.png" alt="Airplane" style ={imageStyle} />
     </div>
   );
 };
