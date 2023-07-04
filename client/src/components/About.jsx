87472213fb2591f0981ee01957d7e46ab42faeb6
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-07-04 13:17:19 +0300

    FLYGO1-6 Updated about section

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import image from "../images/aeroplane.jpg";
+import "./AboutStyle.css";
 
 
 function About(){
   return(
-    <div>
-      <h1>This is the about section of the home page</h1>
+    <div className="about-section">
+    <div className="image-container">
+      <img src={image} alt="About" />
     </div>
+    <div className="content-container">
+      <h2>Travel Any Corner of The World With Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam convallis nisi id nisi ultrices tempus. Nulla et justo vitae dolor porta vestibulum. Donec sollicitudin faucibus ultricies. Fusce consectetur, risus at volutpat mattis, sem nunc aliquet nunc, vitae volutpat ex sapien vitae ipsum.
+      </p>
+      <div className="button-container">
+        <button className="contact-button">Contact us</button>
+      </div>
+    </div>
+  </div>
   );
 }
 
