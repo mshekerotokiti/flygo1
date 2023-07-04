1e6e322cad3c1b1e8413255ce13b4a048350a492
mueni5 <128218860+mueni5@users.noreply.github.com>
2023-07-04 11:49:38 +0300

    Revert "FLYGO1-18 updated item component"

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,39 +1,12 @@
 import React from "react";
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 
 
-const BookingItem = () => {
-  const [booking, setBooking] = useState(null);
-  const { id } = useParams();
-
-  useEffect(() => {
-    // Fetch booking details from the backend API
-    const fetchBooking = async () => {
-      try {
-        const response = await fetch(`/api/bookings/${id}`);
-        const data = await response.json();
-        setBooking(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchBooking();
-  }, [id]);
-
-  if (!booking) {
-    return <div>Loading...</div>;
-  }
-  
+function Item(){
   return(
     <div>
       <h1>This is the item section of the booking item page</h1>
-      <h2>Booking Details</h2>
-      <h3>{booking.user}</h3>
-      <p>{booking.flight}</p>
     </div>
   );
 }
 
-export default BookingItem;
+export default Item;
