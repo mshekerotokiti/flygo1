813ed39556236b4cc117ef9783066fd3a41de5b9
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-07-04 15:10:59 +0300

    added review, header

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Header from "../components/Header";
 import CurrentBookings from "../components/CurrentBookings";
 import PreviousBookings from "../components/PreviousBookings";
 import ReviewsForm from "../components/ReviewsForm";
 import YourReviews from "../components/YourReviews";
+import ProfileNav from "../components/Profilenav";
 
 function Profile(){
   return(
     <div>
-      <Header />
+      <ProfileNav/>
+    
       <CurrentBookings />
       <PreviousBookings />
       <YourReviews />
